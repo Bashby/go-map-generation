88ccf973b1ba5ac589712dfc6f1b76ae434f98eb
Sye <morrowind789@gmail.com>
2017-06-24 00:20:42 -0500

    improving noise lib, added biomes, added moisture, added simple debugging

--- noise/noise.go
+++ noise/noise.go
@@ -2,14 +2,92 @@ package noise
 
 import (
 	"math/rand"
+	"time"
 
 	"github.com/ojrac/opensimplex-go"
 )
 
-// Generate This does something
-func Generate() *opensimplex.Noise {
-	// rand.Seed(time.Now().Unix())
-	noise := opensimplex.NewWithSeed(rand.Int63())
+type FBMOctave struct {
+	frequency float64
+	amplitude float64
+}
+
+type FBMNoiseGenerator struct {
+	generator      *opensimplex.Noise
+	seed           int64
+	octaves        []*FBMOctave
+	baseFrequency  float64
+	baseAmplitude  float64
+	lacunarity     float64
+	gain           float64
+	totalAmplitude float64
+}
+
+func NewFBMOctave(frequency float64, amplitude float64) *FBMOctave {
+	return &FBMOctave{frequency, amplitude}
+}
+
+func NewFBMNoiseGenerator(octaveCount int, baseFrequency float64, baseAmplitude float64, lacunarity float64, gain float64) *FBMNoiseGenerator {
+	// Parse inputs
+	f := baseFrequency
+	a := baseAmplitude
+	l := lacunarity
+	g := gain
+	o := make([]*FBMOctave, octaveCount)
+
+	// Seed the noise generator
+	seed := time.Now().Unix()
+	rand.Seed(seed)
+
+	// Build octaves
+	totalAmplitude := 0.0
+	for i := range o {
+		o[i] = NewFBMOctave(f, a)
+		totalAmplitude += a
+		f *= l
+		a *= g
+	}
+	return &FBMNoiseGenerator{opensimplex.NewWithSeed(rand.Int63()), seed, o, baseFrequency, baseAmplitude, l, g, totalAmplitude}
+}
+
+// ReSeed Seed the generator with a new random seed
+func (n *FBMNoiseGenerator) ReSeed() {
+	newSeed := rand.Int63()
+	n.generator = opensimplex.NewWithSeed(newSeed)
+	n.seed = newSeed
+}
+
+// GenerateFBMNoise Generate a noise value using FBM
+func (n *FBMNoiseGenerator) GenerateFBMNoise(x int, y int) float64 {
+	noise := 0.0
+	for _, octave := range n.octaves {
+		noise += n.generator.Eval2(float64(x)*octave.frequency, float64(y)*octave.frequency) * octave.amplitude
+	}
+
+	// Average noise values across octaves
+	noise /= n.totalAmplitude
 
 	return noise
+}
+
+// BuildNoiseMatrix Builds a 2d matrix of given size filled with simplex noise and Fractal Brownian Motion
+func (n *FBMNoiseGenerator) BuildNoiseMatrix(w int, h int, min int, max int) [][]float64 {
+	data := make([][]float64, w)
+	for x := 0; x < w; x++ {
+		data[x] = make([]float64, h)
+		for y := 0; y < h; y++ {
+			noise := n.GenerateFBMNoise(x, y)
+
+			// fmt.Println(noise)
+
+			// Normalize noise [-1.0, 1.0] to requested range [min, max]
+			noise = float64((max-min))*((noise+0.7)/1.4) + float64(min)
+
+			//noise = noise*float64(max-min)/2 + float64(max+min)/2
+
+			data[x][y] = noise
+		}
+	}
+
+	return data
 }
--- world/world.go
+++ world/world.go
@@ -1,22 +1,88 @@
 package world
 
 import (
-	"math/rand"
+	"image"
+	"image/png"
+	"log"
+	"os"
 
 	"bitbucket.org/ashbyb/go-map-generation/noise"
+
+	"image/color"
 )
 
-// BuildImageNoise Builds a map using simplex noise
-func BuildImageNoise(w int, h int) [][]float64 {
-	r := noise.Generate()
+func Generate(w int, h int) {
+	// Generate Noise
+	noiseGenerator := noise.NewFBMNoiseGenerator(16, .007, 1.0, 2.0, 0.5)
+	elevationNoise := noiseGenerator.BuildNoiseMatrix(w, h, 0, 255)
+	noiseGenerator.ReSeed()
+	moistureNoise := noiseGenerator.BuildNoiseMatrix(w, h, 0, 255)
 
-	data := make([][]float64, w)
-	for x := 0; x < w; x++ {
-		data[x] = make([]float64, h)
-		for y := 0; y < h; y++ {
-			data[x][y] = r.Eval2(rand.Float64(), rand.Float64())
+	// Create an image from target size
+	img := image.NewRGBA(image.Rect(0, 0, w*2, h*2))
+
+	// Load in biome data
+	reader, err := os.Open("world/biomes.v2.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+	biomeData, _, err := image.Decode(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// biomeDataDebug := biomeData
+
+	// Render out the biome debug square
+	debugOffset := 50
+	for x := 0; x < 256; x++ {
+		// Horizontal Lines
+		img.Set(w+x+debugOffset, 0+debugOffset, color.RGBA{255, 0, 0, 255})
+		img.Set(w+x+debugOffset, 255+debugOffset, color.RGBA{255, 0, 0, 255})
+	}
+	for y := 0; y < 256; y++ {
+		// Vertical Lines
+		img.Set(w+debugOffset, y+debugOffset, color.RGBA{255, 0, 0, 255})
+		img.Set(w+255+debugOffset, y+debugOffset, color.RGBA{255, 0, 0, 255})
+	}
+	// biomeDataDebug.Bounds
+
+	// Generate output
+	for i := range elevationNoise {
+		for j := range elevationNoise[i] {
+			elevation := RoundToInt(elevationNoise[i][j])
+			moisture := RoundToInt(moistureNoise[i][j])
+			biome := DetermineBiome(biomeData, elevation, moisture)
+			img.Set(i, j, biome)
+			img.Set(i, j+h, color.RGBA{uint8(elevation), uint8(elevation), uint8(elevation), 255})
+			img.Set(i+w, j+h, color.RGBA{uint8(moisture), uint8(moisture), uint8(moisture), 255})
+			// fmt.Println(elevation, moisture)
+			// Debug Biome
+			img.Set(w+moisture+debugOffset, 255-elevation+debugOffset, color.White)
 		}
 	}
 
-	return data
+	// Save to out.png
+	f, _ := os.OpenFile("out.png", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	defer f.Close()
+	png.Encode(f, img)
+}
+
+func DetermineBiome(biomes image.Image, elevation int, moisture int) color.Color {
+	return biomes.At(moisture, elevation)
+}
+
+func min(x, y int) uint8 {
+	if x < y {
+		return uint8(x)
+	}
+	return uint8(y)
+}
+
+// RoundToInt8 rounds 64-bit floats into integer numbers
+func RoundToInt(a float64) int {
+	if a < 0 {
+		return int(a - 0.5)
+	}
+	return int(a + 0.5)
 }
